32dfcdfdbd6c819507cc3053a01b66724771483e
drcarrasco <dcarrasco@buk.cl>
2022-11-12 17:23:47 -0300

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+#
+# Controlador de páginas estáticas
+#
+class PagesController < ApplicationController
+  def home; end
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+#
+# Helper base de la aplicación
+#
+module ApplicationHelper
+end
